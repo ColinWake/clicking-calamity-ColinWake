aa740a3680f14e03c50606e87fcd89e4a6f702e7
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-03-05 11:17:11 -0500

    Suite Refactoring
    Refactored specs into more specific suites depending on their function in the game
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -3,59 +3,72 @@ describe("Cookie Clicker", () => {
         game = new ClickCounter();
     });
 
-    it('should increment total clicks', () => {
-        game.increment();
-        expect(game.clicks).toBe(1);
+    describe('Clicking', () => {
+
+        it('should increment total clicks', () => {
+            game.increment();
+            expect(game.clicks).toBe(1);
+        });
+
     });
 
-    it('should increase multiplier by 0.2', () => {
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.multiplier).toBe(1.2);
+    describe('Multiplier', () => {
+
+        it('should increase multiplier by 0.2', () => {
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.multiplier).toBe(1.2);
+        });
+
+        it('should square multiplier when > 1.0', () => {
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.multiplier).toBe(1.2);
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.multiplier).toBe(1.44);
+        });
+
+        it('should increase multiplier price by 10%', () => {
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.multiplierPrice).toBe(11);
+        });
+
+        it('should increase price when multiplier is purchased', () => {
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.multiplierPrice).toBe(11)
+        });
+
+        it('should decrease clicks when multiplier is purchased', () => {
+            game.clicks = game.multiplierPrice;
+            game.incrementMultiplier();
+            expect(game.clicks).toBe(0);
+        });
+
     });
 
-    it('should square multiplier when > 1.0', () => {
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.multiplier).toBe(1.2);
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.multiplier).toBe(1.44);
+    describe('Autoclicker', () => {
+
+        it('should increment autoclickers on event call', () => {
+            game.clicks = game.autoClickerPrice;
+            game.incrementAutoClickers();
+            expect(game.autoClickers).toBe(1);
+        });
+
+        it('should increase price when autoclicker is purchased', () => {
+            game.clicks = game.autoClickerPrice;
+            game.incrementAutoClickers();
+            expect(game.autoClickerPrice.toFixed(0)).toBeCloseTo(110);
+        });
+
+        it('should decrease clicks when autoclicker is purchased', () => {
+            game.clicks = game.autoClickerPrice;
+            game.incrementAutoClickers();
+            expect(game.clicks).toBe(0)
+        });
+
     });
 
-    it('should increase multiplier price by 10%', () => {
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.multiplierPrice).toBe(11);
-    });
-
-    it('should increase price when multiplier is purchased', () => {
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.multiplierPrice).toBe(11)
-    });
-
-    it('should decrease clicks when multiplier is purchased', () => {
-        game.clicks = game.multiplierPrice;
-        game.incrementMultiplier();
-        expect(game.clicks).toBe(0);
-    });
-
-    it('should increment autoclickers on event call', () => {
-        game.clicks = game.autoClickerPrice;
-        game.incrementAutoClickers();
-        expect(game.autoClickers).toBe(1);
-    });
-
-    it('should increase price when autoclicker is purchased', () => {
-        game.clicks = game.autoClickerPrice;
-        game.incrementAutoClickers();
-        expect(game.autoClickerPrice.toFixed(0)).toBeCloseTo(110);
-    });
-
-    it('should decrease clicks when autoclicker is purchased', () => {
-        game.clicks = game.autoClickerPrice;
-        game.incrementAutoClickers();
-        expect(game.clicks).toBe(0)
-    });
 });
