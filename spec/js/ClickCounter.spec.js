17b2e71226a5e1232402b66c4069de7a672bf6c4
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-03-04 19:43:34 -0500

    Game Engine Test Suite
    Initial commit, full test suite for all functionality of ClickCounter.js (the game engine script). Bare bones HTML file provided for compatibility with DOM manipulation during the game's run
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

---
+++ spec/js/ClickCounter.spec.js
@@ -0,0 +1,61 @@
+describe("Cookie Clicker", () => {
+    beforeEach(() => {
+        game = new ClickCounter();
+    });
+
+    it('should increment total clicks', () => {
+        game.increment();
+        expect(game.clicks).toBe(1);
+    });
+
+    it('should increase multiplier by 0.2', () => {
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.multiplier).toBe(1.2);
+    });
+
+    it('should square multiplier when > 1.0', () => {
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.multiplier).toBe(1.2);
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.multiplier).toBe(1.44);
+    });
+
+    it('should increase multiplier price by 10%', () => {
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.multiplierPrice).toBe(11);
+    });
+
+    it('should increase price when multiplier is purchased', () => {
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.multiplierPrice).toBe(11)
+    });
+
+    it('should decrease clicks when multiplier is purchased', () => {
+        game.clicks = game.multiplierPrice;
+        game.incrementMultiplier();
+        expect(game.clicks).toBe(0);
+    });
+
+    it('should increment autoclickers on event call', () => {
+        game.clicks = game.autoClickerPrice;
+        game.incrementAutoClickers();
+        expect(game.autoClickers).toBe(1);
+    });
+
+    it('should increase price when autoclicker is purchased', () => {
+        game.clicks = game.autoClickerPrice;
+        game.incrementAutoClickers();
+        expect(game.autoClickerPrice.toFixed(0)).toBe('110');
+    });
+
+    it('should decrease clicks when autoclicker is purchased', () => {
+        game.clicks = game.autoClickerPrice;
+        game.incrementAutoClickers();
+        expect(game.clicks).toBe(0)
+    });
+});
